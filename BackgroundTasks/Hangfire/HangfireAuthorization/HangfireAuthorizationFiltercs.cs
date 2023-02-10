a94115361e30aae18d6e9cefeeb1350b7c222b4b
MahdiAmini98 <81927118+mahdiamini98@users.noreply.github.com>
2023-02-10 10:12:01 +0330

    Added HangfireAuthorizationFiltercs to Permission Dashboard.

---
+++ BackgroundTasks/Hangfire/HangfireAuthorization/HangfireAuthorizationFiltercs.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace Hangfire.HangfireAuthorization
+{
+    public class HangfireAuthorizationFiltercs : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            //دسترسی به همه کاربر هایی که  login کرده اند
+           var httpContext = context.GetHttpContext();
+            return httpContext.User.Identity.IsAuthenticated && httpContext.User.IsInRole("Hangfire");
+        }
+    }
+}
